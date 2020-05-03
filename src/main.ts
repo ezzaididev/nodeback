41f2e197cd5d289e114a5a0620f210a58105192a
ezzaididev <tarik.ezzaidi@gmail.com>
2020-05-03 04:20:42 +0200

    npm install

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
- // const app = await NestFactory.create(AppModule);
- // app.enableCors();
+ const app = await NestFactory.create(AppModule);
+  app.enableCors();
 //  await app.listen(3000);
-  const app = await NestFactory.create(AppModule, { cors: true });
+  //const app = await NestFactory.create(AppModule, { cors: true });
   const options = new DocumentBuilder()
     .setTitle('SPA app')
     .setDescription('The awesome SPA API ')
@@ -14,6 +14,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.port || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
